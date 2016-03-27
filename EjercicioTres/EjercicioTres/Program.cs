1af89b91c1ad2dd36651ea92ab55788366323a13
FacundoLavagnino1993 <facu9396@gmail.com>
2016-03-27 16:00:15 -0300

    Ejercicio tres incompleto

---
+++ EjercicioTres/EjercicioTres/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjercicioTres
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string dato;
+            int num;
+
+            Console.WriteLine("Ingrese un numero:");
+            dato = Console.ReadLine();
+            while(int.TryParse(dato, out num)==false)
+            {
+                Console.WriteLine("ERROR Ingrese un numero:");
+                dato = Console.ReadLine();
+            }
+
+            
+
+
+
+
+        }
+    }
+}
