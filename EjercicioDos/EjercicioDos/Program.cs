ac96e10b14129306af1c4bd0edc139bc4a15b4be
FacundoLavagnino1993 <facu9396@gmail.com>
2016-03-27 15:38:44 -0300

    Ejercicio de la guia

---
+++ EjercicioDos/EjercicioDos/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjercicioDos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string dato;
+            int numero;
+            double cuadrado;
+            double cubo;
+            Console.WriteLine("Ingrese un numero: ");
+            dato = Console.ReadLine();
+            while (int.TryParse(dato, out numero) == false)
+            {
+                Console.WriteLine("ERROR. Ingrese un numero: ");
+                dato = Console.ReadLine();
+            }
+
+            cuadrado = Math.Pow(numero, 2);
+            cubo = Math.Pow(numero, 3);
+            Console.WriteLine("El cuadrado de " + numero + " es: " + cuadrado);
+            Console.WriteLine("EL cubo de " + numero + " es: " + cubo);
+            Console.ReadLine();
+
+        }
+    }
+}
