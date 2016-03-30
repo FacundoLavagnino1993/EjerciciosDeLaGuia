6aeb96069013487f89f98eb1d56c7d2e596b4c86
FacundoLavagnino1993 <facu9396@gmail.com>
2016-03-30 00:57:08 -0300

    Ejercicio 4 incompleto

---
+++ EjercicioCuatro/EjercicioCuatro/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjercicioCuatro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int acumulador = 0;
+            int limite=7;
+            int modulo;
+
+            for(int i=2; i<limite ; i ++ )
+            {
+                for(int j=1; j<i ; j++)
+                {
+                    modulo = i % j;
+                    if(modulo==0)
+                    {
+                        acumulador = acumulador + j;
+                    }
+                }
+
+                if(acumulador == i)
+                {
+                    Console.WriteLine(i + ", ");
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
