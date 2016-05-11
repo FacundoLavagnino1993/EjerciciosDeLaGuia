4c269fb834b8a38bfabe473d56a760386113d2af
unknown <alumno@LAB02PC16.labs.extension>
2016-05-11 10:37:22 -0300

    EjerciciosDeLaGuia

---
+++ Ejercicio41/Ejercicio41/Empleado.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio41
+{
+    class Empleado:SerHumano
+    {
+        private Single _sueldo;
+
+        public Empleado(Single sueldo, string nombre, string sexo)
+            : base(nombre, sexo)
+        {
+            this._sueldo = sueldo;
+        }
+
+        public void Trabajar()
+        {
+            Console.WriteLine("\nEsta trabajando.");
+        }
+
+        public void SueldoMayor()
+        {
+            if (this._sueldo > 500)
+                Console.WriteLine("El sueldo es mayor a 500");
+            else
+                Console.WriteLine("El sueldo es menor a 500");
+        }
+
+    }
+}
---
+++ Ejercicio41/Ejercicio41/SerHumano.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio41
+{
+    class SerHumano
+    {
+        private string _nombre;
+        private Single _peso;
+        private Single _altura;
+        private string _sexo;
+
+        public string Nombre
+        {
+            get { return _nombre; }
+        }
+
+        public string Sexo
+        {
+            get { return _sexo; }
+        }
+
+        public SerHumano(string nombre, string sexo)
+        {
+            this._nombre = nombre;
+            this._sexo = sexo;
+        }
+
+        public SerHumano(Single peso, Single altura, string nombre, string sexo): this(nombre, sexo)
+        {
+            this._peso = peso;
+            this._altura = altura;
+        }
+
+
+        public string Comer()
+        {
+            string rta;
+            rta = "Come mucho";
+            return rta;
+        }
+
+        public void Dormir()
+        {
+            Console.WriteLine("Duerme peola");
+        }
+
+        public void MostrarString()
+        {
+            Console.WriteLine("Nombre : " + Nombre + "\nSexo : " + Sexo);
+        }
+    }
+}
